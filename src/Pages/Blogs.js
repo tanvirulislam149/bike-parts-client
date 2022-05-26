a50d44ea145bf7874494ec3c5866a83fca7975a0
unknown <tanvirulislam149@gmail.com>
2022-05-26 22:38:44 +0600

    5th question done

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -11,7 +11,9 @@ const Blogs = () => {
             <p>3) Que: How does prototypical inheritance work?</p>
             <p>Ans: All the JavaScript object holds a link by which object remains connected like a chain. When you try to access a object, you will see get another object by prototypical inheritance. It means object inherited from another. And you will get so many objects in a chain until it null. All the objects stay connected like chain and can be accessed by inheritance.</p>
             <p>4) Que:  Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts?</p>
-            <p>Ans: React follows a unidirectional data flow. That means if a state </p>
+            <p>Ans: React follows a unidirectional data flow. That means you can only set value of state in one direction and that is by setState. Because React always compares previous data with new data and save the changes and re-renders it. But if you set data directly to a state in won't be able to compare and for this many problems can happen like app crash and etc.   </p>
+            <p>5) Que: What is a unit test? Why should write unit tests?</p>
+            <p>Ans: Unit testing is a software testing system which is performed to watch if a software is working perfectly as expected or not. A unit can be a individual function, module , object etc. The part is needed to test in development process is unit. Unit testing is important because by it we check everything.  </p>
         </div>
     );
 };
