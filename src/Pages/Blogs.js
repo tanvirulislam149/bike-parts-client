54bd04702e049176fe56254ea90238312286afff
unknown <tanvirulislam149@gmail.com>
2022-05-26 21:08:29 +0600

    push issue check

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -4,6 +4,8 @@ const Blogs = () => {
     return (
         <div>
             <p className='text-4xl text-center text-accent-focus'>Question & Answer</p>
+            <p>1) Que:  How will you improve the performance of a React Application?</p>
+            <p>Ans: React compares the actual changes with virtual DOM which is saved in a state. So when the state is changed the children components re-renders. This makes an app slower. so we should use siblings components instead of children to optimize performance. Beside this, we can use React.memo() to memoizing a component. That means any component which is not need rendering will use previous rendered value to optimize performance.</p>
 
         </div>
     );
