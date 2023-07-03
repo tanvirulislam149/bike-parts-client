d5500bad7ad262a78f736b2b8f8d03c3f3c2df56
tanvir <tanvirulislam149@gmail.com>
2023-07-04 00:43:44 +0600

    review done

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -2,26 +2,52 @@ import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
+import ReactStars from "react-rating-stars-component";
+import Loading from "../Loading"
+import { ColorRing } from 'react-loader-spinner';
 
 
 const AddReview = () => {
   const [user, loading, error] = useAuthState(auth);
+  const [rating, setRating] = useState(0);
+  const toastId = React.useRef(null);
+
+  if (loading) {
+    return <Loading />
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const ratings = parseInt(e.target.ratings.value);
-    if (ratings > 5 || ratings < 0) {
-      toast.error("Rating Should Be 0 To 5")
-      return
-    }
-    else {
-      const text = e.target.text.value;
+    const text = e.target.text.value;
+    if (rating == 0) {
+      toast.error("Please Enter Your Rating.")
+      return;
+    } else if (text == "") {
+      toast.error("You Should Describe Your Experience.")
+      return;
+    } else {
+      toast(
+        <div className='flex items-center'>
+          <ColorRing
+            visible={true}
+            height="40"
+            width="40"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={["white", "white", "white", "white", "white"]}
+          />
+          <p>Loading...</p>
+        </div>
+        , { autoClose: false }
+      )
       const review = {
         name: user?.displayName,
         email: user?.email,
-        ratings: ratings,
+        ratings: rating,
         text: text,
       }
+      console.log(review);
       fetch("https://autoparts-vsj8.onrender.com/reviews", {
         method: "POST",
         headers: {
@@ -30,29 +56,51 @@ const AddReview = () => {
         body: JSON.stringify(review)
       })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+          console.log(data);
+          if (data.acknowledged) {
+            toast.dismiss(toastId.current);
+            toast.success("Your review is saved.")
+          }
+        })
     }
     document.getElementById("form").reset();
+    setRating(0);
   }
+
+
+  const ratingChanged = (newRating) => {
+    setRating(newRating);
+    // console.log(newRating);
+  };
 
   return (
     <div>
       <p className='text-5xl rajdhani-font orange-color'>Add Review</p>
-      <form className='md:w-64 mx-auto' id='form' onSubmit={handleSubmit}>
+      <form className='md:w-4/6' id='form' onSubmit={handleSubmit}>
         <label class="label">
-          <span class="label-text">Name</span>
+          <span class="label-text mt-2">Name</span>
         </label>
-        <input type="text" name='email' value={user ? user?.displayName : ""} disabled class="input w-full input-accent input-bordered" /> <br />
+        <input type="text" name='email' value={user ? user?.displayName : ""} disabled class="input w-full rounded-none input-bordered" /> <br />
         <label class="label">
-          <span class="label-text">Email</span>
+          <span class="label-text mt-2">Email</span>
         </label>
-        <input type="text" name='email' value={user ? user?.email : ''} disabled class="input input-accent w-full input-bordered" /> <br />
-        <label class="label">
-          <span class="label-text">Ratings</span>
+        <input type="text" name='email' value={user ? user?.email : ''} disabled class="input rounded-none w-full input-bordered" /> <br />
+        <label class="label pb-0">
+          <span class="label-text mt-2">Ratings</span>
         </label>
-        <input type="number" name='ratings' class="input input-accent w-full input-bordered" /> <br />
-        <textarea name='text' class="textarea textarea-accent w-full my-4 h-24" placeholder="Enter Your text"></textarea> <br />
-        <input className='btn bg-accent-focus w-full border-0' type="submit" value="Submit" />
+        <div className='flex items-center'>
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={40}
+            isHalf={true}
+            activeColor="#FFDE2B"
+          />
+          <p className='bg-black text-white text-center px-3 py-0 ml-3'>{rating}</p>
+        </div>
+        <textarea name='text' class="textarea rounded-none border-red-500 w-full mb-4 h-24" placeholder="Describe Your Experience"></textarea> <br />
+        <input className='btn bg-red-500 hover:bg-black rounded-none w-full border-0' type="submit" value="Submit" />
       </form>
     </div>
   );
--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -9,6 +9,7 @@ import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { BsQuote } from "react-icons/bs";
 import "./Reviews.css"
+import ReactStars from "react-rating-stars-component";
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([]);
@@ -65,13 +66,22 @@ const Reviews = () => {
             reviews.map((r, index) => <SwiperSlide key={index} className="py-10 border-2  border-black bg-white rounded-none text-black">{
               <>
                 <p className="font-bold text-4xl mb-3 rajdhani-font">{r.name}</p>
-                <p className="font-bold text-4xl mb-3 rajdhani-font">{r.ratings}.0</p>
-                <Rating
+                <p className="font-bold text-4xl mb-0 rajdhani-font">{r.ratings.toString().length > 1 ? r.ratings : `${r.ratings}.0`}</p>
+                {/* <Rating
                   initialRating={r.ratings}
                   readonly
                   emptySymbol={<img src="https://i.ibb.co/WyWMbK1/download-removebg-preview.png" className="icon w-5" />}
                   fullSymbol={<img src="https://i.ibb.co/VWnVT22/Star-icon-removebg-preview.png" className="icon w-5" />}
-                />
+                /> */}
+                <div className="flex justify-center">
+                  <ReactStars
+                    edit={false}
+                    value={r.ratings}
+                    size={30}
+                    isHalf={true}
+                    activeColor="#FFDE2B"
+                  />
+                </div>
                 <p className="text-lg pt-2 px-10"><BsQuote className="w-10 h-10" /> {r.text}. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere officiis molestiae! Sint explicabo minus quojkgh.</p>
               </>
             }</SwiperSlide>)
