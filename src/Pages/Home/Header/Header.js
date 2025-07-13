31af1364147a3ca0fa6e5ee2c677f50fe4f5e6de
tanvir <tanvirulislam149@gmail.com>
2025-07-13 21:49:59 +0600

    nav link done

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 import auth from "../../../firebase.init";
 import "./Header.css";
-import logo from "../../../assets/bike-logo.png"
+import logo from "../../../assets/bike-logo.png";
 import { TfiMenuAlt } from "react-icons/tfi";
 
 const Header = () => {
@@ -12,66 +12,139 @@ const Header = () => {
 
   const handleSignOut = () => {
     signOut(auth);
-    localStorage.removeItem('accessToken');
-  }
+    localStorage.removeItem("accessToken");
+  };
 
-  const menu =
+  const menu = (
     <>
-      <li><Link className='md:py-0 font-bold text-xl text-white active:bg-red-500' to="/">Home</Link></li>
-      <li><Link className='md:py-0 font-bold text-xl text-white active:bg-red-500' to="/blogs">Blogs</Link></li>
-      {
-        !user ?
-          <div className="avatar">
-            <div className="w-11 h-11 rounded-full m-auto">
-              <img src="https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg" />
-            </div>
-          </div> : ""
-      }
+      <li>
+        <Link
+          className="md:py-0 font-bold text-xl text-white active:bg-red-500"
+          to="/"
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <a
+          className="md:py-0 font-bold text-xl text-white active:bg-red-500"
+          href="/#parts"
+        >
+          Parts
+        </a>
+      </li>
+      <li>
+        <Link
+          className="md:py-0 font-bold text-xl text-white active:bg-red-500"
+          to="/blogs"
+        >
+          Blogs
+        </Link>
+      </li>
+      {!user ? (
+        <div className="avatar">
+          <div className="w-11 h-11 rounded-full m-auto">
+            <img src="https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg" />
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
+  );
   return (
-    <div className='sticky top-0 z-50 rajdhani-font'>
+    <div className="sticky top-0 z-50 rajdhani-font">
       <div class="px-5 navbar text-white bg-black border-b-4 border-red-500 py-0">
         <div class="navbar">
-          <Link className='text-2xl md:pl-10 orange-color font-bold' to="/">
-            <img className='w-24' src={logo} alt="" />BikeParts
+          <Link className="text-2xl md:pl-10 orange-color font-bold" to="/">
+            <img className="w-24" src={logo} alt="" />
+            BikeParts
           </Link>
         </div>
         <div class="navbar-end w-11/12">
           <div class="dropdown dropdown-end">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
-              <TfiMenuAlt className='w-8 h-8' />
+              <TfiMenuAlt className="w-8 h-8" />
             </label>
-            <ul tabindex="0" class="menu rounded-none dropdown-content p-2 shadow bg-black w-52 mt-4">
+            <ul
+              tabindex="0"
+              class="menu rounded-none dropdown-content p-2 shadow bg-black w-52 mt-4"
+            >
               {menu}
-              {
-                user ? <>
-                  <li><Link className='md:py-0 text-xl active:bg-red-500' to="/dashboard"><button class="font-bold">Dashboard</button></Link></li>
+              {user ? (
+                <>
+                  <li>
+                    <Link
+                      className="md:py-0 text-xl active:bg-red-500"
+                      to="/dashboard"
+                    >
+                      <button class="font-bold">Dashboard</button>
+                    </Link>
+                  </li>
                   <div className="avatar mx-auto">
                     <div className="w-11 h-11 rounded-full">
                       <img src={user.photoURL} />
                     </div>
                   </div>
-                  <li><Link onClick={() => signOut(auth)} className='md:py-0' to=""><button class="md:py-0 text-xl w-full login-btn btn rounded-none active:bg-black">LOG OUT</button></Link></li>
-                </> :
-                  <li><Link className='md:py-0' to="/login"><button class="text-xl w-full login-btn btn active:bg-black rounded-none">LOGIN</button></Link></li>
-              }
+                  <li>
+                    <Link
+                      onClick={() => signOut(auth)}
+                      className="md:py-0"
+                      to=""
+                    >
+                      <button class="md:py-0 text-xl w-full login-btn btn rounded-none active:bg-black">
+                        LOG OUT
+                      </button>
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li>
+                  <Link className="md:py-0" to="/login">
+                    <button class="text-xl w-full login-btn btn active:bg-black rounded-none">
+                      LOGIN
+                    </button>
+                  </Link>
+                </li>
+              )}
             </ul>
           </div>
           <div class="navbar-end w-full hidden lg:flex">
             <ul class="menu menu-horizontal p-0">
               {menu}
-              {
-                user ? <>
-                  <li><Link className='md:py-0 active:bg-red-500' to="/dashboard"><button class="font-bold text-xl">Dashboard</button></Link></li>
+              {user ? (
+                <>
+                  <li>
+                    <Link className="md:py-0 active:bg-red-500" to="/dashboard">
+                      <button class="font-bold text-xl">Dashboard</button>
+                    </Link>
+                  </li>
                   <div className="avatar my-auto">
                     <div className="w-11 h-11 rounded-full">
                       <img src={user.photoURL} />
                     </div>
                   </div>
-                  <li><Link onClick={() => handleSignOut()} className='md:py-0 active:bg-black' to=""><button class="login-btn text-xl rounded-none btn">LOG OUT</button></Link></li>
-                </> :
-                  <li><Link className='md:py-0 active:bg-black' to="/login"><button class="login-btn text-xl rounded-none btn">LOGIN</button></Link></li>
-              }
+                  <li>
+                    <Link
+                      onClick={() => handleSignOut()}
+                      className="md:py-0 active:bg-black"
+                      to=""
+                    >
+                      <button class="login-btn text-xl rounded-none btn">
+                        LOG OUT
+                      </button>
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <li>
+                  <Link className="md:py-0 active:bg-black" to="/login">
+                    <button class="login-btn text-xl rounded-none btn">
+                      LOGIN
+                    </button>
+                  </Link>
+                </li>
+              )}
             </ul>
           </div>
         </div>
--- src/Pages/Home/Parts/Parts.js
+++ src/Pages/Home/Parts/Parts.js
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { useQuery } from 'react-query';
+import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import Loading from "../../Loading";
-import PartsCard from './PartsCard';
+import PartsCard from "./PartsCard";
 import styles from "./Parts.module.css";
 
 const Parts = () => {
-  const { isLoading, error, data: parts } = useQuery('parts', () =>
-    fetch('https://autoparts-vsj8.onrender.com/parts').then(res => {
+  const {
+    isLoading,
+    error,
+    data: parts,
+  } = useQuery("parts", () =>
+    fetch("https://autoparts-vsj8.onrender.com/parts").then((res) => {
       return res.json();
     })
-  )
+  );
 
   if (isLoading) {
-    return <Loading></Loading>
+    return <Loading></Loading>;
   }
 
-
   return (
-    <div>
-      <p className={`text-5xl text-center mt-20 ${styles.title}`}>Parts We Provide</p>
-      <div className='grid grid-cols-1 md:grid-cols-4 gap-10 mb-20 mt-10 mx-3 md:mx-14'>
-        {
-          parts?.map(part => <PartsCard key={part._id} part={part}></PartsCard>)
-        }
+    <div id={"parts"}>
+      <p className={`text-5xl text-center mt-20 ${styles.title}`}>
+        Parts We Provide
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-10 mb-20 mt-10 mx-3 md:mx-14">
+        {parts?.map((part) => (
+          <PartsCard key={part._id} part={part}></PartsCard>
+        ))}
       </div>
     </div>
   );
